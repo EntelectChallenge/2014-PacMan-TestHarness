a822127bc2a9de95197e2571f0c4a21477f9951e
Bernhard Haeussermann <bernhardh@microworks.co.za>
2014-06-21 13:06:39 +0200

    Replace the loop that waits for the output file to exist, with a call to the Process.WaitForExit() method with a time-out.

--- PacManDuel/Models/Player.cs
+++ PacManDuel/Models/Player.cs
@@ -31,7 +31,6 @@ namespace PacManDuel.Models
         {
             var playerOutputFilePath = _workingPath + System.IO.Path.DirectorySeparatorChar + Properties.Settings.Default.SettingBotOutputFileName;
             File.Delete(playerOutputFilePath);
-            var startTime = DateTime.Now;
             var p = new Process
             {
                 StartInfo =
@@ -47,7 +46,6 @@ namespace PacManDuel.Models
                 }
             };
 
-            p.Start();
             System.Diagnostics.DataReceivedEventHandler h = (sender, args) => {
                 using (System.IO.StreamWriter file = new System.IO.StreamWriter(_workingPath + System.IO.Path.DirectorySeparatorChar + "botlogs_capture.txt", true))
                 {
@@ -57,42 +55,29 @@ namespace PacManDuel.Models
             };
             p.OutputDataReceived  += h;
             p.ErrorDataReceived  += h;
-            p.BeginOutputReadLine();
+			p.Start();
+			p.BeginOutputReadLine();
             p.BeginErrorReadLine();
-            try {
-                startTime = p.StartTime; // Adjust for actual start time of process
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            var attemptFetchingMaze = true;
-            while (attemptFetchingMaze)
-            {
-                if (File.Exists(playerOutputFilePath))
-                {
-                    Thread.Sleep(50); // Allow file write to complete, otherwise might get permission exception or corrupt file
-                    if (!p.HasExited) p.Kill();
-                    try
-                    {
-                        var mazeFromPlayer = new Maze(playerOutputFilePath);
-                        return mazeFromPlayer;
-                    }
-                    catch (UnreadableMazeException e)
-                    {
-                        Console.WriteLine(e.ToString());
-                        logFile.WriteLine("[GAME] : Unreadable maze from player " + _playerName);
-                    }
-                }
-                if ((DateTime.Now - startTime).TotalSeconds > Properties.Settings.Default.SettingBotOutputTimeoutSeconds)
-                {
-                    attemptFetchingMaze = false;
-                    if (!p.HasExited) p.Kill();
-                    logFile.WriteLine("[GAME] : Timeout from player " + _playerName);
-                }
-                Thread.Sleep(100);
-            }
-            return null;
+			bool didExit = p.WaitForExit(Properties.Settings.Default.SettingBotOutputTimeoutSeconds * 1000);
+			if (!didExit)
+				p.Kill();
+
+			if (!File.Exists(playerOutputFilePath)) 
+			{
+				logFile.WriteLine("[GAME] : Timeout from player " + _playerName);
+				return null;
+			}
+			try
+			{
+				var mazeFromPlayer = new Maze(playerOutputFilePath);
+				return mazeFromPlayer;
+			}
+			catch (UnreadableMazeException e)
+			{
+				Console.WriteLine(e.ToString());
+				logFile.WriteLine("[GAME] : Unreadable maze from player: " + _playerName);
+			}
+			return null;
         }
 
         public void AddToScore(int score)
