4ef88c020214246633eb7e1f22b997edae21b960
Bernhard Haeussermann <bernhardh@microworks.co.za>
2014-06-24 17:36:40 +0200

    Avoid file-in-use-by-another-process IO exception.

--- PacManDuel/Models/Player.cs
+++ PacManDuel/Models/Player.cs
@@ -47,10 +47,12 @@ namespace PacManDuel.Models
             };
 
             System.Diagnostics.DataReceivedEventHandler h = (sender, args) => {
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(_workingPath + System.IO.Path.DirectorySeparatorChar + "botlogs_capture.txt", true))
+                if (!String.IsNullOrEmpty(args.Data)) 
                 {
-                    if (!String.IsNullOrEmpty(args.Data)) 
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(_workingPath + System.IO.Path.DirectorySeparatorChar + "botlogs_capture.txt", true))
+                    {
                         file.WriteLine(args.Data);
+                    }
                 }
             };
             p.OutputDataReceived  += h;
